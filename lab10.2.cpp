d4ddd9ef5bd1e3404d84175edc47a69276aeb363
Yodelyoda <47818799+Yodelyoda@users.noreply.github.com>
2021-04-15 12:27:47 +0300

    Add files via upload

--- lab10.2.cpp
+++ lab10.2.cpp
@@ -5,18 +5,24 @@
 using namespace std;
 void Change(char* s)
 {
-	FILE * t2;
-
+	FILE*t2;
+	char ch;
 	t2 = fopen("t2.txt", "w");
-
-
+	int j;
 		for (int i = 0; s[i] != ' '; i++)
 		{
 			char ch = s[i];
-				ch = toupper(ch);
+			ch = toupper(ch);
 			fputc(ch, t2);
-
+			j = i;
 		}
+		for (int i = j; s[i] != '\0'; i++)
+		{
+			char ch = s[i];
+			fputc(ch, t2);
+		}
+
+
 }
 
 	
@@ -25,13 +31,13 @@ int main()
 
 	char str1[101];
 	char str2[101];
-	ifstream f("t1.txt");
-
-	f >> str1;
+	ifstream f1("t1.txt");
+	ifstream f2("t1.txt");
+	f1 >> str1;
 	Change(str1);
 	cout << "File t2 has been changed " << endl;
-	f.close();
-
+	f1.close();
+	f2.close();
 	cin.get();
 	return 0;
 }
